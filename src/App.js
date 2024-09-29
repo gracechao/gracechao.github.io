a8c944e498aa881abbe680d8242067db56302012
Henry Chen <henryachen93@gmail.com>
2024-09-29 16:49:09 -0700

    scaffold

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
+  const navItems = ['Home', 'About', 'Contact'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{mx: 3}}>
+         <Typography
+            variant="h5"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }}}
+            style={{ fontFamily: 'Oswald, Arial, sans-sarif' }}
+          >
+            <b>GRACE CHAO</b>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      </Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@fontsource/nunito';
+import '@fontsource/oswald';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Nunito, Arial, sans-serif',
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
